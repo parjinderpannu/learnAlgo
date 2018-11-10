f2539fad07fb6b5b4b8026703d4ae574d629c706
Parjinder Pannu <ppannu@cisco.com>
2018-11-09 23:09:18 -0800

    removed comments

--- learnAlgo-workspace/src/UnsortedLinkedDictionary.java
+++ learnAlgo-workspace/src/UnsortedLinkedDictionary.java
@@ -12,124 +12,64 @@ public class UnsortedLinkedDictionary<K, V>  {
 	
    public V add(K key, V value) {
 	   V result = null;
-//	   boolean found = false;
-//	   Node newNode = new Node(key,value);
-//	   Node currentNode = firstNode;
-//	   
-//	   if(isEmpty()) {
-//		   firstNode = newNode;
-//		   numberOfEntries++;
-//	   }
-//	   else {
-//		   while(!found && (currentNode!=null)) {
-//			   if(currentNode.next == null)
-//				   break;
-//			   if(key.equals(currentNode.getKey())) {
-//				   found = true;
-//				   result = currentNode.getValue();
-//				   currentNode.setValue(value);
-//				   return result;
-//			   }
-//			   else
-//				   currentNode = currentNode.getNextNode();
-//		   } // end while 
-//		   if(currentNode.next==null) {
-//			   currentNode.next = newNode;
-//			   numberOfEntries++;
-//		   }
-//	   }
-//	   
-
-	   
-		 
-		      if( firstNode == null){
-		        Node N = new Node(key,value);
-		        firstNode = N;
-		        numberOfEntries++;
-		      }
-		      else{
-		        Node N = firstNode;
-		        while( N != null){
-		          if(N.next == null){
-		            break;
-		          }
-		          if(key.equals(N.getKey())) {
-					   result = N.getValue();
-					   N.setValue(value);
-					   return result;
-				   }
-		          N = N.next;
-		        }
-		        N.next = new Node(key,value);
-		        numberOfEntries++;
-		      }
+      if( firstNode == null){
+        Node N = new Node(key,value);
+        firstNode = N;
+        numberOfEntries++;
+      }
+      else{
+        Node N = firstNode;
+        while( N != null){
+          if(N.next == null){
+            break;
+          }
+          if(key.equals(N.getKey())) {
+			   result = N.getValue();
+			   N.setValue(value);
 			   return result;
+		   }
+          N = N.next;
+        }
+        N.next = new Node(key,value);
+        numberOfEntries++;
+      }
+      return result;
    } // end add
 
    public V remove(K key) {
 	   V result = null;
 	   int count = 1;
-//	   Node currentNode = firstNode;
 	   Node nodeBefore = null;
-//	   
-//	   if(numberOfEntries ==1 && key.equals(currentNode.getKey())) { //first node
-//		   result = currentNode.getValue();
-//		   firstNode = firstNode.next;
-//		   currentNode.next = null;
-//		   numberOfEntries--;
-//	   }
-//	   else {
-//		   while(!key.equals(currentNode.next.getKey()) && currentNode.next != null) {
-//			   nodeBefore = currentNode;
-//			   currentNode = currentNode.getNextNode();
-//			   count++;
-//		   }
-//		   if(count<numberOfEntries) { // another node except first & last
-//			  result = currentNode.getValue();
-//			  currentNode.next = currentNode.next.next; 
-//			  numberOfEntries--;
-//		   }
-//		   else if(count == numberOfEntries) { // last node
-//			   result = currentNode.getValue();
-//			   currentNode = nodeBefore;
-//			   nodeBefore.next = null;
-//			   numberOfEntries--;
-//		   }
-//		   
-//	   }
-	   
-	 
-		      if(numberOfEntries <= 1){//removes first element
-		        Node N = firstNode;
-		        result = N.getValue();
-		        firstNode = firstNode.next;
-		        N.next = null;
-		        numberOfEntries--;
-		      }
-		      else{//removes the first and anyother element
-		        Node N = firstNode;
-		        if(N.key.equals(key)){
-		        	result = N.getValue();
-		        	firstNode = N.next;
-		          numberOfEntries--;
-		        }
-		        else{
-		          while(!N.next.key.equals(key)){
-		        	    nodeBefore = N;
-		            N = N.next;
-		            count++;
-		            if(count >=numberOfEntries && !N.key.equals(key)) {
-		            		return null;
-		            }		     
-		          }
-		          result = N.getValue();
-		          N.next = N.next.next;
-		          numberOfEntries--;
-		        }
-		      }
-		    
-	   
-   	   return result;
+
+      if(numberOfEntries <= 1){//removes first element
+        Node N = firstNode;
+        result = N.getValue();
+        firstNode = firstNode.next;
+        N.next = null;
+        numberOfEntries--;
+      }
+      else{//removes the first and anyother element
+        Node N = firstNode;
+        if(N.key.equals(key)){
+        	result = N.getValue();
+        	firstNode = N.next;
+          numberOfEntries--;
+        }
+        else{
+          while(!N.next.key.equals(key)){
+        	    nodeBefore = N;
+            N = N.next;
+            count++;
+            if(count >=numberOfEntries && !N.key.equals(key)) {
+            		return null;
+            }		     
+          }
+          result = N.getValue();
+          N.next = N.next.next;
+          numberOfEntries--;
+        }
+      }
+	return result;
    } // end remove
 
    public V getValue(K key) {
